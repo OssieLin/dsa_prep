00e1230ce8283ac288a444c647c76ca538e41190
OssieLin <ossieincase@gmail.com>
2025-06-22 16:54:33 +0800

    add leetcode checker

---
+++ lc.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>  
+using namespace std;
+
+
+class Solution {
+public:
+    
+        
+    }
+};
+
+int main() {
+    vector<int> nums = {};
+
+    Solution sol;  // Create a Solution object
+    // Call the method
+
+    
+    cout << endl;
+
+    return 0;
+}
+

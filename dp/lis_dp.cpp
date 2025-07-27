26bc6dc5667fe83f12f4ad241bef1cafa7f131a9
OssieLin <ossieincase@gmail.com>
2025-07-27 22:27:12 +0800

    update lis

--- dp/lis_dp.cpp
+++ dp/lis_dp.cpp
@@ -7,9 +7,9 @@ int main(){
 	vector<int> v = {2,1,3,8,6,3,9,7,11};
 	vector<int> lis(v.size(), 1);
 	
-	for(int i=v.size()-2; i>=0; --i){
-		for(int j=i+1; j<v.size(); ++j){
-			if(v[j] > v[i]) lis[i] = max(lis[i], lis[j]+1);
+	for(int i=1; i<v.size(); ++i){
+		for(int j=i-1; j>=0; --j){
+			if(v[i] > v[j]) lis[i] = max(lis[i], lis[j]+1);
 		}
 	}
 	
